4dc5a7f7a75f9604857b26d581518b67bcf18788
Andres Hernandez <ahernandez@growthaccelerationpartners.com>
2013-11-13 21:06:59 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecretSanta::Application.config.secret_token = '22ada90bda220301d3f6a7288831eea7c9d04f092bfab0df4b772493e16cf9485d6bc5ead732705468408b3d6ba6dc535aa48d5864ee0641986e829139c2ec66'
